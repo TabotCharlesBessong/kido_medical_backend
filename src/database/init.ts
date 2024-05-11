b2e007aec728594c7828135bb6d946fbed775dfb
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-05-11 12:19:47 +0100

    database setup and some basic configuration handled

--- src/database/init.ts
+++ src/database/init.ts
@@ -1,12 +1,16 @@
-import Db from "./index"
+import TokenModel from "../models/token.model";
+import UserModel from "../models/user.model";
+import Db from "./index";
 
 const DbInitialize = async () => {
   try {
-    console.log("Database connected successfully")
-    await Db.authenticate()
+    console.log("Connected to the database")
+    await Db.authenticate();
+    UserModel.sync({ alter: false });
+    TokenModel.sync({ alter: false });
   } catch (error) {
-    console.log("unablle to connect to database: ", error);
+    console.log("Unable to connect our database", error);
   }
-}
+};
 
-export default DbInitialize
+export default DbInitialize;
